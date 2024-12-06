d5bf1860fa9dd4157036af171dd65db95b76fb0a
SaarGirl <mgollen30@gmail.com>
2024-12-06 17:27:58 +0100

    Delete shared directory

--- shared/js/background/trackers.es6.js
+++
@@ -1,4 +0,0 @@
-const utils = require('./utils.es6')
-const tldts = require('tldts')
-const Trackers = require('@duckduckgo/privacy-grade').Trackers
-module.exports = new Trackers({ tldjs: tldts, utils })
